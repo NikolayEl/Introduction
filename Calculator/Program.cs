96a9afdc0b8af94f4d77f20efdf73c201a703c61
Nikolay Elshin <nikolay19830223@gmail.com>
2023-09-26 17:16:29 +0800

    Calculator is done

---
+++ Calculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+  internal class Program
+  {
+        static void Main(string[] args)
+        {
+            Console.Title = "CALCULATOR";
+            Console.Write("Enter expression: ");
+
+            string expression = Console.ReadLine();
+            string sign = expression;
+            string[] substrings;
+            //Console.WriteLine(expression.IndexOf("+"));
+
+            //Распилил строку по делимитру
+            substrings = expression.Split('+', '-', '*', '/');
+
+            //Перебор получившихся значений и удаление их из строки, пока не останется только знак выражения
+            foreach (var substring in substrings)
+            {
+                sign = sign.Replace(substring, "");
+            }
+
+            //Проверка на наличие указанного знака и на кол-во знаков
+            if(sign == "" || sign.Length > 1)
+            {
+                Console.WriteLine("Не найден знак выражения или их больше одного.");
+                return;
+            }
+
+            //Double потому что могу ввести и int и double
+            double expression_left;
+            double expression_right;
+
+            //Добавил проверку исключений, потому что могут ввести все что угодно
+            try
+            {
+                expression_left = Convert.ToDouble(substrings[0]);
+                expression_right = Convert.ToDouble(substrings[1]);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("Одно или оба значения не являются числами");
+                return;
+            }
+            double result = 0;
+
+            switch (sign)
+            {
+                case "+":
+                    result = expression_left + expression_right;
+                    break;
+                case "-":
+                    result = expression_left - expression_right;
+                    break;
+                case "*":
+                    result = expression_left * expression_right;
+                    break;
+                case "/":
+                    Convert.ToDouble(result);
+                    result = expression_left / expression_right;
+                    break;
+                default:
+                    Console.WriteLine("Неправильный знак или неврно записано выражение!");
+                    break;
+            }
+            Console.WriteLine($"{expression} = {result}");
+            //Console.WriteLine(substrings.Length);
+        }
+        
+  }
+}
---
+++ TasksConvert/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksConvert
+{
+    internal class Program
+    {
+        static readonly string delimitr = "\n-----------------------------------------------------------------------------------\n";
+        static void Main(string[] args)
+        {
+            Console.Title = "Tasks Convert";
+
+            //Task 1
+            Console.WriteLine("Task 1");
+            //1) При помощи разбития без конвертации
+            Console.Write("Enter double number: ");
+            string double_number = Console.ReadLine();
+            string[] substrings = double_number.Split(',', '.');
+            Console.WriteLine($"{substrings[0]} руб. {substrings[1][0]}{(substrings[1].Length <= 1 ? "0" : $"{substrings[1][1]}")} коп.");
+
+            //2) С помощью конвертации
+            decimal number = Convert.ToDecimal(double_number);
+            int rubl = (int)(number);
+            int kopecks = (int)((number - rubl) * 100);
+            Console.WriteLine($"{rubl} руб. {(kopecks < 10 ? $"0{kopecks}":$"{kopecks}" )} коп.");
+            Console.WriteLine(delimitr);
+            //------------------------------------------------------------------------------------------------------------------------------
+
+            //Task 2
+            Console.WriteLine("Task 2");
+
+        } 
+    }
+}
