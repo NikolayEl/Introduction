69214989727563256f1e580264d28f756a9eb97f
Nikolay Elshin <nikolay19830223@gmail.com>
2023-09-27 22:52:09 +0800

    study is done

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -9,6 +9,7 @@ namespace Arrays
 {
     internal class Program
     {
+        static readonly string delimitr = "\n-----------------------------------------------------------------------------\n";
         static void Main(string[] args)
         {
             Console.Write("Enter size array: ");
@@ -21,7 +22,59 @@ namespace Arrays
 
             foreach(int i in arr) Console.Write(i + "\t");
             Console.WriteLine();
+            Console.WriteLine(delimitr);
 
+            Console.Write("Enter number of line: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of row elements: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] i_arr_2 = new int[rows, cols];
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    i_arr_2[i, j] = rand.Next(100);
+                }
+            }
+            for (int i = 0; i < i_arr_2.GetLength(0); i++)
+            {
+                for (int j = 0; j < i_arr_2.GetLength(1); j++)
+                {
+                    Console.Write(i_arr_2[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(i_arr_2.Rank);
+            foreach(int i in i_arr_2) Console.Write(i + "\t");
+            Console.WriteLine();
+            Console.WriteLine(delimitr);
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            int[][] jagged_array = new int[][]
+            {
+                new int[] {3, 5, 8, 13, 21},
+                new int[] {34, 55, 89},
+                new int[] {144, 233, 377, 510}
+            };
+            for(int i = 0; i < jagged_array.Length; i++)
+            {
+                for(int j = 0; j < jagged_array[i].Length; j++) 
+                {
+                    Console.Write(jagged_array[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int[][,] jagged_arr_2 = new int[][,]
+            {
+                i_arr_2,
+                new int[,]
+                {
+                    {256, 384, 512, 768},
+                    {1024, 2048, 3072, 4096}
+                }
+            };
         }
     }
 }
