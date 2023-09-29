b834ed9733be4a6cadc57df3b68799ecd73c651a
Nikolay Elshin <nikolay19830223@gmail.com>
2023-09-29 22:26:58 +0800

    arrays is done Exchange is done

---
+++ Enumerations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    internal class Program
+    {
+        static readonly string delimiter = "\n--------------------------------------------------------\n";
+        static void Main(string[] args)
+        {
+            DayofWeek day = DayofWeek.Friday;
+            Console.WriteLine(day);
+            string[] dayName = Enum.GetNames(typeof(DayofWeek));
+            foreach(string name in dayName) { Console.WriteLine(name); }
+            Console.WriteLine(delimiter);
+
+            DistanceFromSun planet = DistanceFromSun.Earth;
+            Console.WriteLine($"{planet} {planet.GetHashCode()}");
+            string[] distNames = Enum.GetNames(typeof(DistanceFromSun));
+            ulong[] distValues = (ulong[])Enum.GetValues(typeof(DistanceFromSun));
+            for (int i = 0; i < distNames.Length; i++)
+            {
+                Console.WriteLine($"{distNames[i]}\t{distValues[i]}");
+            }
+            Console.WriteLine(Enum.GetUnderlyingType(typeof(DistanceFromSun)).GetType());
+        }
+        enum DayofWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+        enum DistanceFromSun: ulong
+        {
+            Sun = 0,
+            Mercury = 579000,
+            Venus = 108000000,
+            Earth = 149600000
+        };
+    }
+}
---
+++ TransferParametrs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferParametrs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            input(out a, out b);
+            Console.WriteLine($"a = {a}, b = {b}");
+            Exchange(ref a,ref b);
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+        static void input(out int a,out int b)
+        {
+            Console.Write("Enter first a number: ");
+            a = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Enter second a number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        static void Exchange(ref int a,ref int b)
+        {
+            int buffer = a;
+            a = b;
+            b = buffer;
+        }
+    }
+}
