0485e5ec21bf614d162ebc98616f750b1f085c97
Nikolay Elshin <nikolay19830223@gmail.com>
2023-09-30 16:07:02 +0800

    class Fraction is start

---
+++ FractionClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionClass
+{
+    class Fraction
+    {
+        private int integer;
+        private int numerator;
+        private int denominator;
+
+        //----------------------------------Get method's-------------------------------
+        public int getInteger() { return integer; }
+        public int getNumerator() { return numerator;}
+        public int getDenominator() { return denominator;}
+
+        //----------------------------------Set method's-------------------------------
+        public void setInteger(int value) { integer = value; }
+        public void setNumerator(int value) { numerator = value; }
+        public void setDenominator(int value) {  denominator = value; }
+        
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
